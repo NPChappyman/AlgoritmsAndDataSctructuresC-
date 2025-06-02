e411964a4370852c9e59eee83dbdc590d96bb032
HappyMan <100917359+NPChappyman@users.noreply.github.com>
2025-06-02 22:49:19 +0300

    Initial commit

---
+++ Array/Array.cs
@@ -0,0 +1,84 @@
+﻿
+//Insertion O(N)
+//Delete O(N)
+//Find O(N)
+
+namespace AlgoritmsAndDaraStructures.NET.Array
+{
+    internal class Array<T>
+    {
+        private T[] a;
+        private int nElems;
+
+        public Array(int max)
+        {
+            a = new T[max];
+            nElems = 0;
+        }
+
+        public int find(T key)
+        {
+            int j;
+            for (j = 0; j < nElems; j++)
+            {
+                if (a[j].Equals(key))
+                {
+                    return j;
+                }
+            }
+            return -1;
+        }
+        public void insert(T key , int ind )
+        {
+            if (nElems>=a.Length)
+            {
+                T[] arr =new T[a.Length * 2];
+                for (int i = 0; i < nElems; i++)
+                {
+                    arr[i] = a[i];
+                }
+                a = arr;
+
+            }
+            if (nElems==0) {a[0] = key; nElems++; return; }
+           
+            for (int i = nElems-1;i>ind; i--)
+            {
+                a[i+1]=a[i];
+            }
+            a[ind+1] = key;
+            nElems += 1;
+        }
+        public Boolean delete(T key)
+        {
+            if (nElems==0) return false;
+            int j;
+            for (j = 0; j < nElems; j++)
+            {
+                if (a[j].Equals(key))
+                {
+                    break;
+                }
+            }
+            if (a[j].Equals(key))
+            {
+                for (int i = j; i<nElems-1; i++)
+                {
+                    a[i] = a[i+1];
+                }
+                nElems -= 1;
+                return true;
+            }
+            return false;
+        }
+        public void display()
+        {
+            for (int i = 0; i < nElems; i++)
+            {
+                Console.Write(a[i]+" ");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using AlgoritmsAndDaraStructures.NET.Array;
+
+
+Array<int> w = new Array<int>(10);
+w.insert(1, 0);
+w.insert(2, 0);
+w.insert(1, 1);
+w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2); w.insert(8, 2);
+w.insert(9, 1); w.insert(7, 2);
+
+w.display();
+Console.WriteLine(w.find(8));
+w.delete(1);
+w.delete(7);
+w.display();
